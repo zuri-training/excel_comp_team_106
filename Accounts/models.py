b25a1da2c4df5b08afd4688d8e5793e3de11299c
Wilson <oveizon@gmail.com>
2022-08-21 22:09:22 +0100

    test6

--- Accounts/models.py
+++ Accounts/models.py
@@ -2,15 +2,12 @@ from django.db import models
 from django.contrib.auth.models import User
 
 from PIL import Image
-import os
-from django.conf import settings
 
 # Create your models here.
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
 
-    site = os.path.join(settings.MEDIA_ROOT, 'profile_images')
-    avatar = models.ImageField(default='default.jpg', upload_to= site)
+    avatar = models.ImageField(default='default.jpg', upload_to='profile_images')
     bio = models.TextField()
 
     def __str__(self):
