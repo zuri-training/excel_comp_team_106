9f5493f10aa3b0917e206130eda48d01409c05aa
Wilson <oveizon@gmail.com>
2022-09-09 15:24:05 +0100

    design google button

--- XLSWEEP/settings.py
+++ XLSWEEP/settings.py
@@ -33,7 +33,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY=env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env.bool("DEBUG", default=False)
+DEBUG = True#env.bool("DEBUG", default=False)
 
 ALLOWED_HOSTS = ['*']
 
